21e713d70f392a625799b20e5a95f91db8d4fef8
bartcapote <gauclee@gmail.com>
2020-03-03 00:18:03 +0100

    First tkinter tinkering :->
    Import all from tkinter.
    Def myClick.
    Add Label, pack it.
    Add Button, pack it.
    Add mainloop.

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-def test():
-    pass
+from tkinter import *
+
+
+root = Tk()
+
+def myClick():
+    myLabel = Label(root, text="Look! It clicked!")
+    myLabel.pack()
+
+
+myButton = Button(root, text="Clickerino? :->", pad=50)
+myButton.pack()
+
+
+root.mainloop()
